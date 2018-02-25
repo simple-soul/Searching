162226771f1f2f3a9a9b89ea726c5b7c06eb3728
simple_soul <814662638@qq.com>
2018-02-25 19:09:47 +0800

    2

--- app/src/main/java/com/example/simple_soul/searching/MainActivity.kt
+++ app/src/main/java/com/example/simple_soul/searching/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.simple_soul.searching
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jsoup.Jsoup
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hello.setOnClickListener {
+            Toast.makeText(this, "Hello!", Toast.LENGTH_LONG).show()
+            getHtml()
+        }
+
+    }
+
+    fun getHtml()
+    {
+        val t = Thread(runnable).start()
+
+    }
+
+    //搜索地址 https://www.zhizhuso.net/so/[搜索内容]-first-asc-1?f=h
+    private val runnable = Runnable {
+        val doc = Jsoup.connect("https://www.zhizhuso.net/so/中-first-asc-1?f=h").get()
+
+        Log.i("main", doc.title())
     }
 }
